f02b9b0c0baf0fee079f502046d1b912409ac21c
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-03-25 20:56:14 +0100

    Know more popup

--- app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/register/RegisterActivity.java
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/register/RegisterActivity.java
@@ -1,23 +1,38 @@
 package mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.register;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
-import android.content.res.Configuration;
+import android.app.Dialog;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Objects;
 
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
-import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.login.ForgotPasswordActivity;
-import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    CardView donorRegister;
-    CardView volunteerRegister;
-    Configuration newConfig;
+//    CardView donorRegister;
+//    CardView volunteerRegister;
+//    Configuration newConfig;
+    Dialog knowMoreDonorDialog;
+    Dialog knowMoreVolunteerDialog;
+
+    ImageView closePopUpDonor;
+    ImageView closePopUpVolunteer;
+
+    MaterialButton knowMoreDonorBtn;
+    MaterialButton knowMoreDonorOkBtn;
+    MaterialButton knowMoreVolunteerBtn;
+    MaterialButton knowMoreVolunteerOkBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +41,32 @@ public class RegisterActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
 
 //        donorRegister = (CardView) findViewById(R.id.donor_cardview);
 //        volunteerRegister = (CardView) findViewById(R.id.volunteer_cardview);
+          knowMoreDonorDialog =  new Dialog(this);
+          knowMoreDonorBtn =  findViewById(R.id.know_more_donor_button);
+
+          knowMoreVolunteerDialog =  new Dialog(this);
+          knowMoreVolunteerBtn =  findViewById(R.id.know_more_volunteer_button);
+
+
+
+          knowMoreDonorBtn.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  showDonorPopUpDialog();
+              }
+          });
+
+          knowMoreVolunteerBtn.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  showVolunteerPopUpDialog();
+              }
+          });
 //
 //
 //        donorRegister.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +87,54 @@ public class RegisterActivity extends AppCompatActivity {
 //
 //            }
 //        });
+    }
+
+    private void showVolunteerPopUpDialog() {
+
+        knowMoreVolunteerDialog.setContentView(R.layout.volunteer_know_more_popup);
+        closePopUpVolunteer = knowMoreVolunteerDialog.findViewById(R.id.know_more_volunteer_popup_close);
+        knowMoreVolunteerOkBtn =  knowMoreVolunteerDialog.findViewById(R.id.volunteer_know_more_ok_button);
+
+        closePopUpVolunteer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knowMoreVolunteerDialog.dismiss();
+            }
+        });
+
+        Objects.requireNonNull(knowMoreVolunteerDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        knowMoreVolunteerDialog.show();
+
+        knowMoreVolunteerOkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knowMoreVolunteerDialog.dismiss();
+            }
+        });
+    }
+
+
+    public void showDonorPopUpDialog(){
+        knowMoreDonorDialog.setContentView(R.layout.donor_know_more_popup);
+        closePopUpDonor =  knowMoreDonorDialog.findViewById(R.id.know_more_donor_popup_close);
+        knowMoreDonorOkBtn = knowMoreDonorDialog.findViewById(R.id.know_more_donor_ok_button);
+
+        closePopUpDonor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knowMoreDonorDialog.dismiss();
+            }
+        });
+
+        Objects.requireNonNull(knowMoreDonorDialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        knowMoreDonorDialog.show();
+
+        knowMoreDonorOkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                knowMoreDonorDialog.dismiss();
+            }
+        });
     }
 
 

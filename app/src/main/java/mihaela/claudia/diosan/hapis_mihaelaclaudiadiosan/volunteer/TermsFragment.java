f0c07c0ee8c96b3c26564f00acabed5c61ef6e3a
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-03-31 13:43:07 +0200

    termsFragment

--- app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/volunteer/TermsFragment.java
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/volunteer/TermsFragment.java
@@ -1,16 +1,50 @@
 package mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.volunteer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.gcacace.signaturepad.views.SignaturePad;
+import com.google.android.material.button.MaterialButton;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
 
 public class TermsFragment extends Fragment {
+
+    TextView termsTV;
+    View view;
+
+    private static  final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+    SignaturePad signaturePad;
+    MaterialButton mClearButton;
+    MaterialButton mSaveButton;
 
     public TermsFragment() {
         // Required empty public constructor
@@ -20,6 +54,158 @@ public class TermsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terms, container, false);
+        view = inflater.inflate(R.layout.fragment_terms, container, false);
+
+        termsTV = view.findViewById(R.id.terms_textView);
+        termsTV.setMovementMethod(new ScrollingMovementMethod());
+
+        signaturePad = view.findViewById(R.id.signature_pad);
+        signaturePad.setOnSignedListener(new SignaturePad.OnSignedListener() {
+            @Override
+            public void onStartSigning() {
+               // Toast.makeText(getActivity(), "On Start Signing", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSigned() {
+                mClearButton.setEnabled(true);
+                mSaveButton.setEnabled(true);
+            }
+
+            @Override
+            public void onClear() {
+                mClearButton.setEnabled(false);
+                mSaveButton.setEnabled(false);
+            }
+        });
+
+
+        mClearButton = view.findViewById(R.id.clearSignatureButton);
+        mSaveButton = view.findViewById(R.id.saveSignatureButton);
+
+        mClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signaturePad.clear();
+            }
+        });
+
+
+        mSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bitmap signatureBitmap = signaturePad.getSignatureBitmap();
+                verifyStoragePermissions(getActivity());
+
+                if (addJpgSignatureToGallery(signatureBitmap)) {
+                    Toast.makeText(getActivity(), getString(R.string.signature_saved_jpg), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.unable_to_store_jpg), Toast.LENGTH_SHORT).show();
+                }
+                if (addSvgSignatureToGallery(signaturePad.getSignatureSvg())) {
+                    Toast.makeText(getActivity(), getString(R.string.svg_signature_saved), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.unable_to_store_svg), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return view;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length <= 0
+                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), getString(R.string.cannot_write_external), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.e(getString(R.string.signature_pad), getString(R.string.directory_not_created));
+        }
+        return file;
+    }
+
+    public void saveBitmapToJPG(Bitmap bitmap, File photo) throws IOException {
+        Bitmap newBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(newBitmap);
+        canvas.drawColor(Color.WHITE);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        OutputStream stream = new FileOutputStream(photo);
+        newBitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream);
+        stream.close();
+    }
+
+    public boolean addJpgSignatureToGallery(Bitmap signature) {
+        boolean result = false;
+        try {
+            File photo = new File(getAlbumStorageDir(getString(R.string.signature_pad)), String.format("Signature_%d.jpg", System.currentTimeMillis()));
+            saveBitmapToJPG(signature, photo);
+            scanMediaFile(photo);
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void scanMediaFile(File photo) {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri contentUri = Uri.fromFile(photo);
+        mediaScanIntent.setData(contentUri);
+        getActivity().sendBroadcast(mediaScanIntent);
+    }
+
+    public boolean addSvgSignatureToGallery(String signatureSvg) {
+        boolean result = false;
+        try {
+            File svgFile = new File(getAlbumStorageDir(getString(R.string.signature_pad)), String.format("Signature_%d.svg", System.currentTimeMillis()));
+            OutputStream stream = new FileOutputStream(svgFile);
+            OutputStreamWriter writer = new OutputStreamWriter(stream);
+            writer.write(signatureSvg);
+            writer.close();
+            stream.flush();
+            stream.close();
+            scanMediaFile(svgFile);
+            result = true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p/>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity the activity from which permissions are checked
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+
 }

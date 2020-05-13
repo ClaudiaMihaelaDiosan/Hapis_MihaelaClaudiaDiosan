533a3aa8ec284796d6ba09629fed5af423415d02
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-05-14 00:02:56 +0200

    configuration fragments finished

--- app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/donor/ConfigurationDonorFragment.java
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/donor/ConfigurationDonorFragment.java
@@ -2,32 +2,39 @@ package mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.donor;
 
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
+import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.register.RegisterDonorActivity;
 
 import static android.content.Context.MODE_PRIVATE;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ConfigurationDonorFragment extends Fragment {
+public class ConfigurationDonorFragment extends Fragment implements View.OnClickListener{
 
-    /*Preferences*/
-    private SharedPreferences preferences;
+
 
     /*EditText*/
     private TextInputEditText phoneNumber;
@@ -38,6 +45,9 @@ public class ConfigurationDonorFragment extends Fragment {
     /*View fragment*/
     private View view;
 
+    /*Firebase*/
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser user;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,13 +55,30 @@ public class ConfigurationDonorFragment extends Fragment {
 
         view = inflater.inflate(R.layout.configuration_fragment, container, false);
 
-        preferences = getActivity().getSharedPreferences("userInfo", MODE_PRIVATE);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mFirestore = FirebaseFirestore.getInstance();
 
         initViews();
-        saveButton();
 
         return view;
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        saveConfigBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.config_configuration_button_save){
+            changePhone();
+        }
+    }
+
 
 
     private void initViews(){
@@ -59,26 +86,17 @@ public class ConfigurationDonorFragment extends Fragment {
         saveConfigBtn = view.findViewById(R.id.config_configuration_button_save);
     }
 
-    private void saveButton(){
-        saveConfigBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isValidPhoneNumber(phoneNumber.getText().toString())){
-                    changePhone();
-                    Toast.makeText(getActivity(), getString(R.string.config_changed_phone_toast), Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getActivity(), getString(R.string.phone_error_text), Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
-    }
 
     public void changePhone(){
-        String phoneNumberValue = phoneNumber.getText().toString();
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("donorPhone", phoneNumberValue);
-        editor.apply();
+
+        String document = user.getEmail();
+
+        if (!isValidPhoneNumber(phoneNumber.getText().toString())){
+            phoneNumber.setError(getString(R.string.phone_error_text));
+        }else{
+            mFirestore.collection("donors").document(document).update("donorPhone", phoneNumber.getText().toString());
+            showSuccessToast(getString(R.string.config_changed_phone_toast));
+        }
     }
 
     public  boolean isValidPhoneNumber(CharSequence target) {
@@ -89,20 +107,17 @@ public class ConfigurationDonorFragment extends Fragment {
         }
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig){
-        super.onConfigurationChanged(newConfig);
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE || newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
-            try {
-                FragmentTransaction ft = getFragmentManager().beginTransaction();
-                if (Build.VERSION.SDK_INT >= 26) {
-                    ft.setReorderingAllowed(false);
-                }
-                ft.detach(this).attach(this).commit();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+    public void showSuccessToast(String message){
+        Toast toast = Toast.makeText(getContext(), message, Toast.LENGTH_LONG);
+        View view =toast.getView();
+        view.setBackgroundColor(Color.WHITE);
+        TextView toastMessage =  toast.getView().findViewById(android.R.id.message);
+        toastMessage.setTextColor(Color.BLUE);
+        toastMessage.setGravity(Gravity.CENTER);
+        toastMessage.setTextSize(15);
+        toastMessage.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_check_circle_black_24dp,0,0,0);
+        toastMessage.setPadding(10,10,10,10);
+        toast.show();
     }
 
 }
--- app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/volunteer/ConfigurationVolunteerFragment.java
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/volunteer/ConfigurationVolunteerFragment.java
@@ -2,29 +2,33 @@ package mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.volunteer;
 
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
 
 import static android.content.Context.MODE_PRIVATE;
 
-public class ConfigurationVolunteerFragment extends Fragment {
+public class ConfigurationVolunteerFragment extends Fragment implements View.OnClickListener {
 
-
-    /*Preferences*/
-    private SharedPreferences preferences;
 
     /*EditText*/
     private TextInputEditText phoneNumber;
@@ -36,45 +40,61 @@ public class ConfigurationVolunteerFragment extends Fragment {
     private View view;
 
 
+    /*Firebase*/
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser user;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.configuration_fragment, container, false);
 
-        preferences = getActivity().getSharedPreferences("userInfo", MODE_PRIVATE);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mFirestore = FirebaseFirestore.getInstance();
+
         initViews();
-        saveButton();
 
         return view;
     }
 
-    private void saveButton() {
-        saveConfigBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (isValidPhoneNumber(phoneNumber.getText().toString())){
-                    changePhone();
-                    Toast.makeText(getActivity(), getString(R.string.config_changed_phone_toast), Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getActivity(), getString(R.string.phone_error_text), Toast.LENGTH_SHORT).show();
-                }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-            }
-        });
+        saveConfigBtn.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.config_configuration_button_save){
+            changePhone();
+        }
+    }
+
+    public void changePhone(){
+
+        String document = user.getEmail();
+
+        if (!isValidPhoneNumber(phoneNumber.getText().toString())){
+            phoneNumber.setError(getString(R.string.phone_error_text));
+        }else{
+            mFirestore.collection("volunteers").document(document).update("volunteerPhone", phoneNumber.getText().toString());
+            showSuccessToast(getString(R.string.config_changed_phone_toast));
+        }
+    }
+
+
 
     private void initViews() {
         phoneNumber = view.findViewById(R.id.configuration_phone_number);
         saveConfigBtn = view.findViewById(R.id.config_configuration_button_save);
     }
 
-    public void changePhone(){
-        String phoneNumberValue = phoneNumber.getText().toString();
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("volunteerPhone", phoneNumberValue);
-        editor.apply();
-    }
+
 
     public  boolean isValidPhoneNumber(CharSequence target) {
         if (target== null || target.length() < 6 || target.length() > 13) {
@@ -83,6 +103,20 @@ public class ConfigurationVolunteerFragment extends Fragment {
             return android.util.Patterns.PHONE.matcher(target).matches();
         }
     }
+
+    public void showSuccessToast(String message){
+        Toast toast = Toast.makeText(getContext(), message, Toast.LENGTH_LONG);
+        View view =toast.getView();
+        view.setBackgroundColor(Color.WHITE);
+        TextView toastMessage =  toast.getView().findViewById(android.R.id.message);
+        toastMessage.setTextColor(Color.BLUE);
+        toastMessage.setGravity(Gravity.CENTER);
+        toastMessage.setTextSize(15);
+        toastMessage.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_check_circle_black_24dp,0,0,0);
+        toastMessage.setPadding(10,10,10,10);
+        toast.show();
+    }
+
 
 
 }

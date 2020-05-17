68640522979b181b4190f5951ef30a66c4104552
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2020-05-17 23:17:03 +0200

    HomeVolunteerFragment, DonorAdapter

---
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/donor/DonorAdapter.java
@@ -0,0 +1,94 @@
+package mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.donor;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
+import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.homeless.Homeless;
+import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.volunteer.HomelessAdapter;
+
+public class DonorAdapter extends FirestoreRecyclerAdapter<Homeless, DonorAdapter.DonorAdaperHolder>{
+
+    private DonorAdapter.OnItemClickListener mListener;
+
+    public interface OnItemClickListener{
+        void onItemClick(DocumentSnapshot  documentSnapshot, int position);
+    }
+
+    public void setOnItemClickListener(DonorAdapter.OnItemClickListener listener){
+        mListener = listener;
+    }
+
+    public DonorAdapter(@NonNull FirestoreRecyclerOptions<Homeless> options) {
+
+        super(options);
+
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull DonorAdaperHolder donorHolder, int i, @NonNull Homeless homeless) {
+
+        donorHolder.username.setText(homeless.getHomelessUsername());
+        donorHolder.locationAddress.setText(homeless.getHomelessAddress());
+        donorHolder.need.setText(homeless.getHomelessNeed());
+
+
+        // Uri.parse(homeless.getImage())
+        Glide
+                .with(donorHolder.itemView.getContext())
+                .load(homeless.getImage())
+                .placeholder(R.drawable.no_profile_image)
+                .into(donorHolder.profileImageView);
+    }
+
+    @NonNull
+    @Override
+    public DonorAdaperHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.homeless_card_custom_view, parent, false);
+        return new DonorAdaperHolder(view, mListener);
+    }
+
+
+    class DonorAdaperHolder extends RecyclerView.ViewHolder{
+
+        ImageView profileImageView;
+        TextView username;
+        TextView locationAddress;
+        TextView need;
+
+        public DonorAdaperHolder(@NonNull View itemView, final OnItemClickListener listener) {
+            super(itemView);
+
+            profileImageView = itemView.findViewById(R.id.profile_image_donor_card);
+            username = itemView.findViewById(R.id.homeless_username_tv_donor_card);
+            need = itemView.findViewById(R.id.homeless_need_tv_donor_card);
+            locationAddress = itemView.findViewById(R.id.homeless_locationAddress_tv_donor_card);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemClick(getSnapshots().getSnapshot(position), position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+}
--- app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/donor/HomeDonorFragment.java
+++ app/src/main/java/mihaela/claudia/diosan/hapis_mihaelaclaudiadiosan/donor/HomeDonorFragment.java
@@ -11,10 +11,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.R;
 import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.homeless.Homeless;
+import mihaela.claudia.diosan.hapis_mihaelaclaudiadiosan.volunteer.HomelessAdapter;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -23,14 +31,14 @@ public class HomeDonorFragment extends Fragment {
 
     private View view;
 
-    private ArrayList<Homeless> homelessList;
 
     private SharedPreferences preferences;
 
-    public HomeDonorFragment() {
-        // Required empty public constructor
-    }
+    private FirebaseFirestore mFirestore;
+    private FirebaseUser user;
 
+    private CollectionReference homelessRef;
+    private DonorAdapter donorAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,144 +48,61 @@ public class HomeDonorFragment extends Fragment {
 
         preferences = getActivity().getSharedPreferences("homelessInfo", MODE_PRIVATE);
 
-        //Recyclerview
-      /*  addItems();*/
+        firebaseInit();
         buildRecyclerView();
 
         return view;
     }
 
     public void buildRecyclerView(){
-        RecyclerView mrecyclerView = view.findViewById(R.id.recycler_view_donor);
-        mrecyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(view.getContext());
-        AdapterHome mAdapter = new AdapterHome(homelessList);
+        homelessRef = mFirestore.collection("homeless");
 
-        mrecyclerView.setLayoutManager(mLayoutManager);
-        mrecyclerView.setAdapter(mAdapter);
+        FirestoreRecyclerOptions<Homeless> options = new FirestoreRecyclerOptions.Builder<Homeless>()
+                .setQuery(homelessRef, Homeless.class)
+                .build();
 
-        mAdapter.setOnItemClickListener(new AdapterHome.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position) {
-                HomelessFragment homelessFragment = new HomelessFragment();
-                SharedPreferences.Editor editor = preferences.edit();
-                editor.putInt("position", position);
-                editor.apply();
-                getItems();
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.donor_fragment_container, homelessFragment)
-                        .addToBackStack(null).commit();
-            }
-        });
+        donorAdapter = new DonorAdapter(options);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view_donor);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(donorAdapter);
+
+       donorAdapter.setOnItemClickListener(new DonorAdapter.OnItemClickListener() {
+           @Override
+           public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+               String username =  documentSnapshot.getString("homelessUsername");
+               SharedPreferences.Editor editor = preferences.edit();
+               editor.putString("homelessUsername", username);
+               editor.apply();
+
+               HomelessFragment homelessFragment = new HomelessFragment();
+               getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.donor_fragment_container, homelessFragment)
+                       .addToBackStack(null).commit();
+
+           }
+       });
+
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        donorAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        donorAdapter.stopListening();
+    }
 
 
+    private void firebaseInit(){
+        mFirestore = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
-/*    public void addItems(){
-        homelessList = new ArrayList<>();
-
-        homelessList.add(new Homeless(R.drawable.andrew_image,"Andrew",getString(R.string.work), getString(R.string.andrew_location)));
-        homelessList.add(new Homeless(R.drawable.maria_image,"Maria",getString(R.string.food), getString(R.string.maria_location)));
-        homelessList.add(new Homeless(R.drawable.maite_image,"Maite",getString(R.string.clothes), getString(R.string.maite_location)));
-        homelessList.add(new Homeless(R.drawable.luis_image,"Luis",getString(R.string.work), getString(R.string.luis_location)));
-        homelessList.add(new Homeless(R.drawable.cristina_image,"Cristina",getString(R.string.lodging), getString(R.string.cristina_location)));
-    }*/
-
-    public void getItems(){
-
-        String andrewName = "Andrew";
-        String andrewBirthday = "12/02/1980";
-        String andrewStory = getString(R.string.andrew_story);
-        String andrewLocation = getString(R.string.andrew_location);
-        String andrewLatitude = "41.6119987";
-        String andrewLongitude = "0.6242844";
-        String andrewSchedule =  getString(R.string.andrew_schedule);
-        String andrewNeed = getString(R.string.work);
-
-
-        String mariaName = "Maria";
-        String mariaBirthday = "06/03/1985";
-        String mariaStory = getString(R.string.maria_story);
-        String mariaLocation = getString(R.string.maria_location);
-        String mariaLatitude = "41.611801";
-        String mariaLongitude = "0.628162";
-        String mariaSchedule =  getString(R.string.maria_schedule);
-        String mariaNeed = getString(R.string.food);
-
-        String maiteName = "Maite";
-        String maiteBirthday = "03/05/1960";
-        String maiteStory = getString(R.string.maite_story);
-        String maiteLocation = getString(R.string.maite_location);
-        String maiteLatitude = "41.6116452";
-        String maiteLongitude = "0.6318808";
-        String maiteSchedule =  getString(R.string.maite_schedule);
-        String maiteNeed = getString(R.string.clothes);
-
-        String luisName = "Luis";
-        String luisBirthday = "07/01/1979";
-        String luisStory = getString(R.string.luis_story);
-        String luisLocation = getString(R.string.luis_location);
-        String luisLatitude = "41.620809";
-        String luisLongitude = "0.628363";
-        String luisSchedule =  getString(R.string.luis_schedule);
-        String luisNeed = getString(R.string.work);
-
-        String cristinaName = "Cristina";
-        String cristinaBirthday = "09/12/1975";
-        String cristinaStory = getString(R.string.cristina_story);
-        String cristinaLocation = getString(R.string.cristina_location);
-        String cristinaLatitude = "41.6169655";
-        String cristinaLongitude = "0.6132156";
-        String cristinaSchedule =  getString(R.string.cristina_schedule);
-        String cristinaNeed = getString(R.string.work);
-
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("andrewName", andrewName);
-        editor.putString("andrewBirthday", andrewBirthday);
-        editor.putString("andrewStory", andrewStory);
-        editor.putString("andrewLocation", andrewLocation);
-        editor.putString("andrewLatitude", andrewLatitude);
-        editor.putString("andrewLongitude", andrewLongitude);
-        editor.putString("andrewSchedule", andrewSchedule);
-        editor.putString("andrewNeed", andrewNeed);
-
-        editor.putString("mariaName", mariaName);
-        editor.putString("mariaBirthday", mariaBirthday);
-        editor.putString("mariaStory", mariaStory);
-        editor.putString("mariaLocation", mariaLocation);
-        editor.putString("mariaLatitude", mariaLatitude);
-        editor.putString("mariaLongitude", mariaLongitude);
-        editor.putString("mariaSchedule", mariaSchedule);
-        editor.putString("mariaNeed", mariaNeed);
-
-        editor.putString("maiteName", maiteName);
-        editor.putString("maiteBirthday", maiteBirthday);
-        editor.putString("maiteStory", maiteStory);
-        editor.putString("maiteLocation", maiteLocation);
-        editor.putString("maiteLatitude", maiteLatitude);
-        editor.putString("maiteLongitude", maiteLongitude);
-        editor.putString("maiteSchedule", maiteSchedule);
-        editor.putString("maiteNeed", maiteNeed);
-
-        editor.putString("luisName", luisName);
-        editor.putString("luisBirthday", luisBirthday);
-        editor.putString("luisStory", luisStory);
-        editor.putString("luisLocation", luisLocation);
-        editor.putString("luisLatitude", luisLatitude);
-        editor.putString("luisLongitude", luisLongitude);
-        editor.putString("luisSchedule", luisSchedule);
-        editor.putString("luisNeed", luisNeed);
-
-        editor.putString("cristinaName", cristinaName);
-        editor.putString("cristinaBirthday", cristinaBirthday);
-        editor.putString("cristinaStory", cristinaStory);
-        editor.putString("cristinaLocation", cristinaLocation);
-        editor.putString("cristinaLatitude", cristinaLatitude);
-        editor.putString("cristinaLongitude", cristinaLongitude);
-        editor.putString("cristinaSchedule", cristinaSchedule);
-        editor.putString("cristinaNeed", cristinaNeed);
-        editor.apply();
     }
 
 }
